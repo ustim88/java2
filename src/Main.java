451e1493d3b6eb16b69722abba2c11c3ee630ba9
ustim88 <138520920+ustim88@users.noreply.github.com>
2023-11-06 21:06:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int price = 10_000;
         int miles = price / 20;
-        System.out.println("бонус: "+ miles);
+        System.out.println("бонус: " + miles);
     }
 }
